bac7feaa07f149cdd191dbf10315bc9ae0f308f4
Uros Minoski <urosminoski9@gmail.com>
2023-12-11 17:17:13 +0100

    Makefile updated, added execution of python file also.

--- fixPointBlockFir/python/plotResults.py
+++ fixPointBlockFir/python/plotResults.py
@@ -1,5 +1,7 @@
+import wave
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 def read_pcm_file(file_path):
     with open(file_path, 'rb') as pcm_file:
@@ -8,24 +10,50 @@ def read_pcm_file(file_path):
 
         return pcm_data
 
-# Input data.
-file_path_input = r"../data/input.pcm"
-pcm_input = read_pcm_file(file_path_input)
-# Output data.
-file_path_output = r"../data/output.pcm"
-pcm_output = read_pcm_file(file_path_output)
+def get_sampling_frequency(wav_file_path):
+    with wave.open(wav_file_path, 'rb') as wave_file:
+        # Get the sample rate from the WAV file
+        sample_rate = wave_file.getframerate()
+    return sample_rate
 
-# Assuming 16-bit signed PCM data
-pcm_input_array = np.frombuffer(pcm_input, dtype=np.int16)
-pcm_output_array = np.frombuffer(pcm_output, dtype=np.int16)
+if __name__ == "__main__":
+    # Getting sampling frequency from input wav file.
+    wav_file_path = r"../data/input.wav"
+    fs = get_sampling_frequency(wav_file_path)
+    print(f"Sampling Frequency: {fs} Hz")
 
-FFT_input = np.fft.fft(pcm_input_array, 4096)
-FFT_output = np.fft.fft(pcm_output_array, 4096)
+    # Input data.
+    file_path_input = r"../data/input.pcm"
+    pcm_input = read_pcm_file(file_path_input)
+    # Output data.
+    file_path_output = r"../data/output.pcm"
+    pcm_output = read_pcm_file(file_path_output)
 
-# Plotting
-plt.figure()
-# plt.plot(np.abs(FFT_input)[:len(FFT_input)//2], label="Input")
-plt.plot(np.abs(FFT_output)[:len(FFT_output)//2], label="Output")
-plt.legend()
-plt.grid()
-plt.show()
+    # Assuming 16-bit signed PCM data
+    pcm_input_array = np.frombuffer(pcm_input, dtype=np.int16)
+    pcm_output_array = np.frombuffer(pcm_output, dtype=np.int16)
+
+    FFT_input = np.fft.fft(pcm_input_array, 4096)
+    FFT_output = np.fft.fft(pcm_output_array, 4096)
+
+    x_axis = np.arange(len(FFT_input)//2) / (len(FFT_input)//2) * fs / 2 / 1e3
+    y_axis1 = np.abs(FFT_input)[:len(FFT_input)//2]
+    y_axis2 = np.abs(FFT_output)[:len(FFT_output)//2]
+
+    xticks_values = np.linspace(0, fs / 2 / 1e3, 15)
+    # Plotting
+    plt.figure(figsize = (1920 / 100, 1080 / 100))
+    plt.plot(x_axis, y_axis1, label="Input")
+    plt.plot(x_axis, y_axis2, label="Output")
+    plt.xticks(xticks_values)
+    plt.xlabel("Frequency [kHz]")
+    plt.ylabel("DFT Amplitude")
+    plt.title("Input and output (filtered) spectrum")
+    plt.legend()
+    plt.grid()
+
+    plt.savefig(r"..\plots\plot.png")
+
+    plt.show()
+
+
--- fixPointBlockFir/src/main.c
+++ fixPointBlockFir/src/main.c
@@ -9,51 +9,73 @@ Int16 w[C_NUM_TAPS];        // Circular buffer.
 Int16 x[C_NUM_BLOCK_DATA],  // Input block data.
       y[C_NUM_BLOCK_DATA];  // Output block data.
 
-void main(){
-    FILE *fpIn, *fpOut;     // Input and output file pointers.
+int main(){
+    FILE *fpIn1, *fpOut1,
+         *fpIn2, *fpOut2;     // Input and output file pointers.
 
     Int8 dataTmp[2 * C_NUM_BLOCK_DATA];    // Temporary variable to store 1B input and output data.
 
     Int8 key;
 
     printf("Example -- fixed-point block FIR filtering\n");
-    printf("Enter 1 for PCM and 2 for WAV file\n");
-    scanf("%d", &key);
 
-    if(key == 1){
-        fpIn    = fopen("../data/input.pcm", "rb");
-        fpOut   = fopen("../data/output.pcm", "wb");
-    }
-    else if(key == 2){
-        fpIn    = fopen("../data/input.wav", "rb");
-        fpOut   = fopen("../data/output.wav", "wb");
+    fpIn1 = fopen("../data/input.pcm", "rb");
+    fpIn2 = fopen("../data/input.wav", "rb");
+
+    if(fpIn1 != NULL){
+        fpOut1 = fopen("../data/output.pcm", "wb");
     }
     else{
-        perror("Invalid key entered!");
-        exit(0);
+        perror("Error while opening input PCM file!");
+        return 1;
     }
-
-    if(fpIn == NULL){
-        perror("Error while opening input file!");
-        exit(0);
-    }
-    else if(fpOut == NULL){
-        perror("Error while opening output file!");
-        exit(0);
-    }
-
-    if(key == 2){
+    
+    if(fpIn2 != NULL){
         Uint8 wavHeader[44];
-        fread(wavHeader, sizeof(Int8), 44, fpIn);  // Read header form input wav file.
-        fwrite(wavHeader, sizeof(Int8), 44, fpOut); // Write read header to output wav file.
+        fpOut2 = fopen("../data/output.wav", "wb");
+        fread(wavHeader, sizeof(Int8), 44, fpIn2);  // Read header form input wav file.
+        fwrite(wavHeader, sizeof(Int8), 44, fpOut2); // Write read header to output wav file.
     }
+    else
+        printf("Unable to open WAV file. Program will continue wirking with PCM!");
+
+    // printf("Enter 1 for PCM and 2 for WAV file\n");
+    // scanf("%d", &key);
+
+    // if(key == 1){
+    //     fpIn    = fopen("../data/input.pcm", "rb");
+    //     fpOut   = fopen("../data/output.pcm", "wb");
+    // }
+    // else if(key == 2){
+    //     fpIn    = fopen("../data/input.wav", "rb");
+    //     fpOut   = fopen("../data/output.wav", "wb");
+    // }
+    // else{
+    //     perror("Invalid key entered!");
+    //     return 1;
+    // }
+
+    // if(fpIn == NULL){
+    //     perror("Error while opening input file!");
+    //     return 1;
+    // }
+    // else if(fpOut == NULL){
+    //     perror("Error while opening output file!");
+    //     return 1;
+    // }
+
+    // if(key == 2){
+    //     Uint8 wavHeader[44];
+    //     fread(wavHeader, sizeof(Int8), 44, fpIn);  // Read header form input wav file.
+    //     fwrite(wavHeader, sizeof(Int8), 44, fpOut); // Write read header to output wav file.
+    // }
 
     /* Initializing circular buffer (w) to zero. */
     for(Uint16 i = 0; i < C_NUM_TAPS; i++){
         w[i] = 0;
     }
 
-    while(fread(dataTmp, sizeof(Int8), 2 * C_NUM_BLOCK_DATA, fpIn) == 2 * C_NUM_BLOCK_DATA){
+    while(fread(dataTmp, sizeof(Int8), 2 * C_NUM_BLOCK_DATA, fpIn1) == 2 * C_NUM_BLOCK_DATA){
 
         /* Converting 1B input data into int16. Data is little-endian. */
         for(Uint16 j = 0, i = 0; i < C_NUM_BLOCK_DATA; i++){
@@ -71,11 +93,16 @@ void main(){
             j += 2; 
         }
 
-        /* Write output to the output file. */
-        fwrite(dataTmp, sizeof(Int8), 2 * C_NUM_BLOCK_DATA, fpOut);
+        /* Write output to the output files. */
+        fwrite(dataTmp, sizeof(Int8), 2 * C_NUM_BLOCK_DATA, fpOut1);
+        fwrite(dataTmp, sizeof(Int8), 2 * C_NUM_BLOCK_DATA, fpOut2);
     }
 
     /* Close files. */
-    fclose(fpIn);
-    fclose(fpOut);
+    fclose(fpIn1);
+    fclose(fpIn2);
+    fclose(fpOut1);
+    fclose(fpOut2);
+
+    return 0;
 }
